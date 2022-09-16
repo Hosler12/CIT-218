afca25cc2c8a06936b6b7b43e3922828341f9e65
Hosler12 <Hosler12@mail.nmc.edu>
2022-09-15 18:42:34 -0700

    lesson 02

---
+++ DetectiveAgency-master/Models/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+    
+namespace DetectiveAgency.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "Please enter your Id")]
+        public int? Id { get; set; }
+        [StringLength(30, ErrorMessage ="Name cannot be longer than 30 characters")]
+        public string? Name { get; set; }
+        [Required (ErrorMessage="Please enter your hours")]
+        [Range(10, 75, ErrorMessage = "Hours must be between 10 and 75.")]
+        public double? Hours { get; set; }
+        [Required (ErrorMessage = "Please enter your rate")]
+        [Range(15, 75, ErrorMessage = "Rate should be between $15 and $75")]
+        public double? Rate { get; set; }
+        public double? CalcSalary()
+        {
+            double? grosspay = 0;
+            double? overtime = 0;
+            double? regPay = 0;
+            if (Hours > 40)
+            {
+                regPay = 40 * Rate;
+                overtime = (Hours - 40) * Rate * 1.5;
+            }
+            else
+            {
+                regPay = Hours * Rate;
+            }
+            grosspay = regPay + overtime;
+            return grosspay;
+        }
+    }
+}
---
+++ FutureValue/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FutureValue.Models;
+
+namespace FutureValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.CalculateFutureValue();
+            }
+            else
+            {
+                ViewBag.FV = 0;
+            }
+            return View(model);
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}

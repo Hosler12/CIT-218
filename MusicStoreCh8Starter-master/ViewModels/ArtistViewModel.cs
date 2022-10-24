f6060584c4d09e110e4cc420bc98ad02fd1854d2
Hosler12 <Hosler12@mail.nmc.edu>
2022-10-24 10:39:18 -0700

    Chapter 08

---
+++ MusicStoreCh8Starter-master/ViewModels/ArtistViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MusicStore.Models;
+using System.Collections.Generic;
+
+namespace MusicStore.ViewModels
+{
+    public class ArtistViewModel
+    {
+        public List<Album> Album { get; set; }
+        public List<Artist> Artist { get; set; }
+    }
+}
